5750d01ef21d88d543de417cc6700e77a39d192c
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-05-13 18:19:09 +0530

    end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LinkedInComponent } from './linked-in/linked-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'linkedIn',component:LinkedInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LinkedInComponent } from './linked-in/linked-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkedInComponent
   ],
   imports: [
     BrowserModule,
